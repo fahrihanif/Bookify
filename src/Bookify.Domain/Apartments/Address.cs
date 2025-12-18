12dd0e341521bd03153ae7bd534749dae29abf5a
Fahri Hanif <fahrihanif03@gmail.com>
2025-12-18 19:05:19 +0700

    chore: configure code style with my preferences

--- src/Bookify.Domain/Apartments/Address.cs
+++ src/Bookify.Domain/Apartments/Address.cs
@@ -1,9 +1,9 @@
 namespace Bookify.Domain.Apartments;
 
 public record Address(
-    string Street, 
-    string State, 
-    string ZipCode, 
-    string City, 
+    string Street,
+    string State,
+    string ZipCode,
+    string City,
     string Country
 );
--- src/Bookify.Domain/Apartments/Money.cs
+++ src/Bookify.Domain/Apartments/Money.cs
@@ -5,11 +5,11 @@ public record Money(decimal Amount, Currency Currency)
     public static Money operator +(Money first, Money second)
     {
         if (first.Currency != second.Currency)
-        {
             throw new InvalidOperationException("Currencies must match");
-        }
+
         return new Money(first.Amount + second.Amount, first.Currency);
     }
 
-    public static Money Zero() => new(0, Currency.None);
+    public static Money Zero()
+        => new(0, Currency.None);
 }
